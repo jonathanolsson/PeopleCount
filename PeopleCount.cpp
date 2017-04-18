4c4aac99ab881dfa5e5b4d00bbc3064238dd3587
jool1403 <jool1403@student.miun.se>
2017-04-18 13:58:44 +0200

    Initial commit

---
+++ PeopleCount.cpp
@@ -0,0 +1,15 @@
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include "Test.h"
+
+/** @function main */
+int main(int argc, char** argv)
+{
+
+
+
+
+	return 0;
+}
